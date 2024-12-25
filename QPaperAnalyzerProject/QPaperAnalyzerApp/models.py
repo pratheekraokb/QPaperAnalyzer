7b45a86fbc321a484c5f9f0773db837dafd13ec0
= <pratheekmulleria@gmail.com>
2024-12-25 10:55:54 +0530

    QPaper and Questions Module created

--- QPaperAnalyzerProject/QPaperAnalyzerApp/models.py
+++ QPaperAnalyzerProject/QPaperAnalyzerApp/models.py
@@ -17,3 +17,28 @@ class Course(models.Model):
     def __str__(self):
         return self.coursecode  # Returning course code as string representation
 
+class QPaper(models.Model):
+    EXAM_TYPE_CHOICES = [
+        ('Supply', 'Supply'),
+        ('Regular', 'Regular'),
+    ]
+
+    QPaper_ID = models.AutoField(primary_key=True)
+    CourseCode = models.CharField(max_length=8)
+    Max_Marks = models.IntegerField(default=100)
+    Exam_Type = models.CharField(max_length=7, choices=EXAM_TYPE_CHOICES, default='Regular')
+    Exam_Name = models.CharField(max_length=400)
+
+    def __str__(self):
+        return f"{self.CourseCode} - {self.Exam_Name}"
+
+class QPaperQuestions(models.Model):
+    ID = models.AutoField(primary_key=True)
+    QPaper_ID = models.ForeignKey(QPaper, on_delete=models.CASCADE, related_name="questions")
+    QuestionText = models.TextField()
+    Mark = models.IntegerField(default=1)
+    Topic = models.TextField()
+    Module_Number = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"Question {self.ID} for QPaper {self.QPaper_ID}"
