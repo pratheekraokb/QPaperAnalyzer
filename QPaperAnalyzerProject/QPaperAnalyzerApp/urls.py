3c3de2d4cf0480a4d28ce8f06b71edb361362617
= <pratheekmulleria@gmail.com>
2025-01-12 08:59:35 +0530

    minor changes

--- QPaperAnalyzerProject/QPaperAnalyzerApp/urls.py
+++ QPaperAnalyzerProject/QPaperAnalyzerApp/urls.py
@@ -5,10 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('api/getTopicsSyllabus/<str:CourseCode>/<int:Module>/', views.API_get_topics_syllabus, name='API_get_topics_syllabus'),
     path('api/QuestionsToTopic/', views.API_question_to_topic, name='API_question_to_topic'),
-    path('api/QPaperExcelToDB/', views.API_QPaperExcelToDB, name='API_QPaperExcelToDB'),
-    path('api/getQuestionsTopicsAnswer/<int:QPaperID>/', views.API_QuestTopicAns, name='API_QuestTopicAns'),
-    
+    path('api/QPaperExcelToDB/', views.API_QPaperExcelToDB, name='API_QPaperExcelToDB'), #POST
     path('addCoursesThroughCSV/', views.dataEntryFunc, name='dataEntryFunc'),
+    path('api/getQuestionsTopicsAnswer/<int:QPaperID>/', views.API_QuestTopicAns, name='API_QuestTopicAns'),
     path('upload/', views.upload_file, name='upload_file'),
 
     # Authentication
@@ -23,11 +22,6 @@ urlpatterns = [
     path('qPaperAnalyze/<int:QPaper1ID>/', views.WEB_QPaperAnalysis ,name='WEB_QPaperAnalysis'),
     path('student/qPaperUpload/', views.qPaperUpload, name='qPaperUpload'),
     path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
-
-    
-
-    path('faculty/dashboard/', views.faculty_dashboard, name='faculty_dashboard'),
-
-    
-
+    # path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('faculty/dashboard/', views.faculty_dashboard, name='faculty_dashboard'),   
 ]
